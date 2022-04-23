88999e487c01d0a67a89c04cba5769197314b301
TungNguyen <tung.nk41@gmail.com>
2022-04-23 09:27:18 +0700

    update

--- CustomBottomSheet/app/src/main/java/com/example/custombottomsheet/BottomSheetModalFragment.kt
+++ CustomBottomSheet/app/src/main/java/com/example/custombottomsheet/BottomSheetModalFragment.kt
@@ -1,9 +1,11 @@
 package com.example.custombottomsheet
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetModalFragment: BottomSheetDialogFragment() {
@@ -13,6 +15,7 @@ class BottomSheetModalFragment: BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dialog?.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         return inflater.inflate(R.layout.bottom_sheet_modal,container,false)
     }
 }
