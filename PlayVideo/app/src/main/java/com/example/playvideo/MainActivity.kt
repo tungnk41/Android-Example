99956c736848ff431aafb00289f1e59c0e7196de
tuhn <tung.nk41@gmail.com>
2022-08-07 23:54:15 +0700

    init play video

---
+++ PlayVideo/app/src/main/java/com/example/playvideo/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.playvideo
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Player.*
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DataSpec
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var playerView : StyledPlayerView
+    private lateinit var videoPlayer : ExoPlayer
+    private lateinit var btnPlayPause : Button
+    private lateinit var progressBar : ProgressBar
+    private var isPlaying: Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        playerView = findViewById(R.id.playerView)
+        btnPlayPause = findViewById(R.id.btnPlayPause)
+        progressBar = findViewById(R.id.progressBar)
+        videoPlayer = ExoPlayer.Builder(this).build()
+        prepare()
+
+        btnPlayPause.setOnClickListener {
+            isPlaying = !isPlaying
+            setPlayButtonState(isPlaying)
+            if(isPlaying) {
+                play()
+            }
+            else {
+                pause()
+            }
+        }
+
+        videoPlayer.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                when(playbackState) {
+                    STATE_IDLE -> {
+                        isPlaying = true
+                        setPlayButtonState(isPlaying)
+                    }
+                    STATE_BUFFERING -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    STATE_READY -> {
+                        progressBar.visibility = View.GONE
+                    }
+                    STATE_ENDED -> {
+                        isPlaying = false
+                        setPlayButtonState(isPlaying)
+                        videoPlayer.seekTo(0)
+                        videoPlayer.playWhenReady = false
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onPause() {
+        if (isPlaying) {
+            videoPlayer.pause()
+        }
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isPlaying) {
+            videoPlayer.play()
+        }
+    }
+
+
+    override fun onDestroy() {
+        videoPlayer.stop()
+        videoPlayer.release()
+        super.onDestroy()
+    }
+
+
+    private fun prepare() {
+        val uri = RawResourceDataSource.buildRawResourceUri(R.raw.test)
+        val rawResourceDataSource = RawResourceDataSource(this)
+        try {
+            rawResourceDataSource.open(DataSpec(uri))
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val factory: DataSource.Factory = DataSource.Factory { rawResourceDataSource }
+        val mediaSource = ProgressiveMediaSource.Factory(factory)
+            .createMediaSource(MediaItem.fromUri(uri))
+        videoPlayer.setMediaSource(mediaSource)
+        videoPlayer.prepare()
+        videoPlayer.playWhenReady = false
+        playerView.player = videoPlayer
+    }
+
+    private fun play() {
+        videoPlayer.play()
+    }
+
+    private fun pause() {
+        videoPlayer.pause()
+    }
+
+    private fun setPlayButtonState(isPlaying : Boolean) {
+        if(isPlaying) {
+            btnPlayPause.text = "Pause"
+        }
+        else {
+            btnPlayPause.text = "Play"
+        }
+    }
+
+}
