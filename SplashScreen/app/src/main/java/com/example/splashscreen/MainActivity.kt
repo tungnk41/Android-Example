10c12148c9dd9281681eeb74fcf0da0cb07d41b0
tungnk41 <tung.nk41@gmail.com>
2023-02-22 13:56:15 +0700

    update

--- SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -5,12 +5,11 @@ import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
-    private val a = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
             setKeepOnScreenCondition{
-                a == 1
+                true
             }
         }
         setContentView(R.layout.activity_main)
